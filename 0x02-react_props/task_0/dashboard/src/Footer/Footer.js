45edfc670e935f4cd86618dd4638d96438dadc03
khlifaelbad@gmail.com <khlifaelbad@gmail.com>
2024-11-09 02:08:02 +0100

    up-to-date'

---
+++ 0x02-react_props/task_0/dashboard/src/Footer/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+import { getFullYear, getFooterCopy } from "../utils/utils";
+
+function Footer() {
+  return (
+    <>
+      <div className="App-footer">
+        Copyright {getFullYear()} - {getFooterCopy()}
+      </div>
+    </>
+  );
+}
+
+export default Footer;
+
+
